61b6aac97dcf012926d439c6ef93fdee31088338
ian0000 <niklas0617@gmail.com>
2021-05-22 21:19:39 -0500

    Add files via upload
    ahora si ya final

--- bd_proyecto.sql
+++ bd_proyecto.sql
@@ -103,6 +103,5 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
-
 INSERT INTO tbasevotantes VALUES('9999999999','Master','Root');
-INSERT INTO tpartidopolitico(par_logo, par_nombre) VALUES ('null','NULO');
+INSERT INTO `tpartidopolitico`(`par_id`, `par_logo`, `par_nombre`) VALUES (1,'null','NULO');
